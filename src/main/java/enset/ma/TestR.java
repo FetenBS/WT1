179c6216c0d00be681017325018c361c0c857225
fetenBS <fetenbenslimen@gmail.com>
2022-05-16 16:23:59 +0200

    InitialCommit

---
+++ src/main/java/enset/ma/TestR.java
@@ -0,0 +1,6 @@
+package enset.ma;
+public class TestR{
+    public static void main(String[] args) {
+        System.out.print("Hi");
+    }
+}
---
+++ src/main/java/ma/enset/TestFIFI.java
@@ -0,0 +1,7 @@
+package enset.ma;
+public class TestFIFI{
+public static void main(String[] args) {
+    System.out.println("Hi FIFI");
+}
+    
+}
